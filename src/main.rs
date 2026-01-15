ff758a3fc533595f67dbb97da04609ce25582484
kenedy <kenedymirandadasilva@gmail.com>
2026-01-14 22:48:24 -0300

    feat: add initial menu system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod add_book;
+enum Menu {
+    AddBook,
+    BookList,
+    BorrowBook,
+    ReturnBook,
+}
+
+pub struct Book {
+    name: String,
+    author: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut library: Vec<Book> = Vec::new();
+
+    let choice:Menu = Menu::AddBook;
+    match choice {
+        Menu::AddBook => {},
+        Menu::BookList => {},
+        Menu::BorrowBook => {},
+        Menu::ReturnBook => {},
+    }
 }
